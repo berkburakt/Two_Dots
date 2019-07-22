e09d1385aab8e56b552927dc91c21db17f916cd8
ege-yildirim <ege.yildirimm24@gmail.com>
2019-07-22 19:58:54 +0300

    First update
    Rotation via mouse movement
    Added sample background and ball sprites

---
+++ Two Dots 2D/Assets/Scripts/Rotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speed = 500f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+
+
+        //transform.Rotate(0, 0, Time.deltaTime * speed); // Testing if i rotates properly
+
+         
+    }
+}
---
+++ Two Dots 2D/Assets/Scripts/randomSpawn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomSpawn : MonoBehaviour
+
+
+{
+    public GameObject Foodprefab;
+
+    public Vector3 center;
+    public Vector3 size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnFood();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.Q))
+        //{
+
+        //    SpawnFood();
+        //}
+    }
+
+    public void SpawnFood()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2));
+
+        Instantiate(Foodprefab, pos, Quaternion.identity);
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+}
