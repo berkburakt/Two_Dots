6bd940b455b3f6b3e634d9034da009f05f0a4ac4
Berk Burak Taşdemir <burakt546@live.com>
2019-07-22 20:44:16 +0300

    Adding Enemy Ball

---
+++ Two Dots 2D/Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+
+{
+    public GameObject EnemyBall;
+
+    public Vector3 center;
+    public Vector3 size;
+
+    public int spawnNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int i;
+        for (i = 0; i < spawnNumber; i++)
+        {
+            spawnBody(EnemyBall);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void spawnBody(GameObject EnemyBall)
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), 0, Random.Range(-size.z / 2, size.z / 2));
+        GameObject clone = Instantiate(EnemyBall, pos, Quaternion.Euler(0,0,0));
+    }
+}
