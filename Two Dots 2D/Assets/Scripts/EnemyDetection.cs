68e63a6d7fce6571e58b665c528cb6e171c38901
ege-yildirim <ege.yildirimm24@gmail.com>
2019-07-24 00:22:08 +0300

    Added Screen Schake On Collision
    Added screen shake effect for colliding with enemy balls.

--- Two Dots 2D/Assets/Scripts/EnemyDetection.cs
+++ Two Dots 2D/Assets/Scripts/EnemyDetection.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyDetection : MonoBehaviour
 {
+    public GameObject cameraShake;
+
+    public bool isCollided;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,28 @@ public class EnemyDetection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      
         
+    }
+
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 originalPos = cameraShake.transform.localPosition;
+
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            cameraShake.transform.localPosition = new Vector3(x, y, originalPos.z);
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null; //waits untill next frame
+        }
+        cameraShake.transform.localPosition = originalPos;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -22,7 +46,9 @@ public class EnemyDetection : MonoBehaviour
         {
             if(collision.transform.tag == "EnemyGreen")
             {
+                StartCoroutine(Shake(0.1f, 0.1f));
                 Destroy(collision.gameObject);
+                
             } else
             {
 
@@ -31,12 +57,16 @@ public class EnemyDetection : MonoBehaviour
         {
             if (collision.transform.tag == "EnemyPurple")
             {
+                StartCoroutine(Shake(0.1f, 0.1f));
                 Destroy(collision.gameObject);
+                
             }
             else
             {
 
             }
         }
+
+
     }
 }
